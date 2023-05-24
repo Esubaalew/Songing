02398f0adfd18281faa1fc117d2948f722609fc2
Esubaalew <106541640+Esubaalew@users.noreply.github.com>
2023-05-24 19:33:16 +0300

    Updates

---
+++ precache-manifest.f4a154c2f1c8f123800f9feef11e9eff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98c056ee621a5713ea2d3b9f3cd9ca0",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "3eef1419f6210d9c604c",
+    "url": "/songs/static/js/2.e82c6137.chunk.js"
+  },
+  {
+    "revision": "bd1f1de3cec90387633b",
+    "url": "/songs/static/js/main.d0c63ceb.chunk.js"
+  },
+  {
+    "revision": "dcf58ff1c08ee33d2636",
+    "url": "/songs/static/js/runtime~main.4612f727.js"
+  }
+]);
